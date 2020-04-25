a55db9bcd1bf4ec0901089cf2d9a527f6afce40e
Kamil <ase495@gmail.com>
2020-04-25 21:06:45 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 sudo apt-get update
 sudo apt-get install build-essential
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 12.13.0
+
+
 sudo apt-get install git
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 git clone https://github.com/kozikkam/azure-performance-tests.git
+cd azure-performance-tests/
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+npm install
+npm run start:prod
