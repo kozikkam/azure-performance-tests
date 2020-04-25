a03137fbfac14443cb94d1ca91e2036ec1695d05
Kamil <ase495@gmail.com>
2020-04-25 22:51:21 +0200

    Split to prebuild and deploy

---
+++ buildImage.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt-get install git
--- setup.sh
+++ setup.sh
@@ -1,13 +1,3 @@
-sudo apt-get update
-sudo apt-get install build-essential
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 12.13.0
-
-
-sudo apt-get install git
 git clone https://github.com/kozikkam/azure-performance-tests.git
 cd azure-performance-tests/
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
