86ab2de949bab33af38217b44968a71d17dc151b
Kamil <ase495@gmail.com>
2020-04-26 20:17:12 +0200

    Update

--- buildImage.sh
+++ buildImage.sh
@@ -1,5 +1,4 @@
 sudo apt-get update
 sudo apt-get install build-essential
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo apt-get install git
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install git
 git clone https://github.com/kozikkam/azure-performance-tests.git
 cd azure-performance-tests/
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-npm install
+npm i
 npm run start:prod
