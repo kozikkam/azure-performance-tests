6a2ebbc9480b7574e35f02e929041720a6607a59
Kamil <ase495@gmail.com>
2020-04-26 18:51:40 +0200

    Setup fix

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt-get install git
 git clone https://github.com/kozikkam/azure-performance-tests.git
 cd azure-performance-tests/
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
